3e261ef756e413e5b90e70fd32df273bfc276829
RGAA-Software <hollow.ichigo.yoo@gmail.com>
2023-12-16 22:52:58 +0800

    rename
    Signed-off-by: RGAA-Software <hollow.ichigo.yoo@gmail.com>

--- tc_encoder.cpp
+++ tc_encoder.cpp
@@ -2,7 +2,7 @@
 // Created by RGAA on 2023-12-16.
 //
 
-#include "TCEncoder.h"
+#include "tc_encoder.h"
 
 namespace tc
 {
---
+++ tc_encoder.h
@@ -0,0 +1,18 @@
+//
+// Created by RGAA on 2023-12-16.
+//
+
+#ifndef TC_ENCODER_TC_ENCODER_H
+#define TC_ENCODER_TC_ENCODER_H
+
+
+namespace tc
+{
+
+    class TCEncoder {
+
+    };
+
+}
+
+#endif //TC_ENCODER_TC_ENCODER_H
